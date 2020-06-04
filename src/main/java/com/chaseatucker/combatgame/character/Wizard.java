c483bcf20bb49d0faac9ccc4966993d2c47feb82
Chase Tucker <tuck8106@gmail.com>
2020-06-04 09:43:54 -0700

    Wizard test completed

--- src/main/java/com/chaseatucker/combatgame/character/Wizard.java
+++ src/main/java/com/chaseatucker/combatgame/character/Wizard.java
@@ -29,8 +29,8 @@ public class Wizard extends Character{
     public int attack() {
         int index = (int) (Math.random() * this.getAttackSkills().size());
         AttackSkill attackSkill = this.getAttackSkills().get(index);
-        int attackStrength = attackSkill.getAttackStrength() * fatigueStrength();
-        System.out.println(this.getName() + "(" + this.getClass().getSimpleName() + ")" +
+        int attackStrength = attackSkill.getAttackStrength() - fatigueStrength();
+        System.out.println(this.getName() + " (" + this.getClass().getSimpleName() + ")" +
                 " attacks with " + attackSkill.name() + " with " + attackStrength +
                 " points of damage.");
         return attackStrength;
@@ -44,19 +44,27 @@ public class Wizard extends Character{
     public int defend() {
         int index = (int) (Math.random() * this.getDefendSkills().size());
         DefendSkill defendSkill = this.getDefendSkills().get(index);
-        int defendStrength = defendSkill.getDefendStrength() * fatigueStrength();
-        System.out.println(this.getName() + "(" + this.getClass().getSimpleName() + ")" +
+        int defendStrength = defendSkill.getDefendStrength() - fatigueStrength();
+        System.out.println(this.getName() + " (" + this.getClass().getSimpleName() + ")" +
                 " defends with " + defendSkill.name() + " with " + defendStrength +
                 " defense points.");
         return defendStrength;
     }
 
+    /**
+     * Wizard special move method.
+     * @return int[] with index 0 being attack value and index 1 being defend value.
+     */
     @Override
     public int[] special() {
         return new int[] {this.attack(), this.defend()};
     }
 
+    /**
+     * Private method to determine how much fatigue effects attack and defend effectiveness.
+     * @return
+     */
     private int fatigueStrength() {
-        return (int) Math.round(this.getHealth() * 0.7);
+        return (int) Math.round(Math.floor((100 - this.getHealth()) * 0.07));
     }
 }
